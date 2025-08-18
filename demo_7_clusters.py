e9794e0ae454ff375ec8e1701f7f7d63a5a998d1
emirseners <senerahmetemir@gmail.com>
2025-08-18 14:39:11 +0300

    last version12312

--- demo_7_clusters.py
+++ demo_7_clusters.py
@@ -5,14 +5,13 @@ from simclstr.clusterer import read_time_series, perform_clustering
 import os
 
 def main():
-    model_path_stella2 = os.path.expanduser('~/Desktop/SESDYN_Basic_A/Basic_A_Stella2.xlsx')
+    model_path_vensim = os.path.expanduser('~/Desktop/SESDYN_Basic_A/Basic_A_Vensim.xlsx')
 
-    read_data_stella2 = read_time_series(model_path_stella2)
+    read_data_vensim = read_time_series(model_path_vensim)
 
-    clustering_results_stella2 = perform_clustering(read_data_stella2, cMethod='maxclust', cValue = 7, plotDendrogram=True)
+    clustering_results_vensim = perform_clustering(read_data_vensim, cMethod='maxclust', cValue = 6, plotDendrogram=True)
 
-    interactive_plot_clusters(clustering_results_stella2[1], 'pattern_dtw')
-
+    interactive_plot_clusters(clustering_results_vensim[1], 'pattern_dtw')
 
 if __name__ == "__main__":
     main()
